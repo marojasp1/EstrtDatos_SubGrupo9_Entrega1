be843d477c18735b78b88051a7d6adf3ca989f38
marojasp1 <miguel.rojas343@yahoo.com>
2025-03-25 19:48:04 -0400

    Nombres de los integrantes agregados
    Nombres de los integrantes agregados

--- Principal.java
+++ Principal.java
@@ -1,9 +1,12 @@
 public class Principal {
 
     //Subgrupo 9
+    //Doris Guzman Licona
+    //Jonathan Eduardo Ortiz Palacios
+    //Daniel Felipe Rincon Labrador
     //Miguel Alejandro Rojas Perez
-    //Daniel felipe rincon labrador
     //Sebastian Valencia Sierra
+    
 
     public static void main(String[] args) {
         Ejercicios ej = new Ejercicios();
